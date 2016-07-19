c5660c64008595bf83400c7f4d6870c851b4b2aa
Mike Ulvila <mikeulvila@me.com>
2016-07-19 14:39:21 -0500

    create contact function

--- main.js
+++ main.js
@@ -52,11 +52,16 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
 	};
 
 	$scope.showCreateModal = function () {
+		$scope.contacts.selectedPerson = {};
 		$scope.createModal = $modal({
 			scope: $scope,
 			template: 'templates/modal.create.tpl.html',
 			show: true
 		})
+	};
+
+	$scope.createContact = function () {
+		$scope.contacts.createContact($scope.contacts.selectedPerson);
 	};
 
 	$scope.$watch('search', function(newVal, oldVal) {
@@ -147,6 +152,12 @@ app.service('ContactService', ['Contact', function (Contact) {
 				var index = self.persons.indexOf(person);
 				self.persons.splice(index, 1);
 				self.selectedPerson = null;
+			});
+		},
+		'createContact': function (person) {
+			self.isSaving = true;
+			Contact.save(person).$promise.then(function() {
+				self.isSaving = false;
 			});
 		}
 
