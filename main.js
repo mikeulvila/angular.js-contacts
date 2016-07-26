74b7ec9aafa4d06ea26d8b9827ba3d22e42fa215
Mike Ulvila <mikeulvila@me.com>
2016-07-25 22:32:03 -0500

    add routes

--- main.js
+++ main.js
@@ -11,6 +11,43 @@ var app = angular.module('codecraft', [
 	'ui.router'
 ]);
 
+app.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('list', {
+			url: "/",
+			views: {
+				'main': {
+					templateUrl: 'templates/list.html',
+					controller: 'PersonListController'
+				},
+				'search': {
+					templateUrl: 'templates/searchform.html',
+					controller: 'PersonListController'
+				}
+			}
+		})
+		.state('edit', {
+			url: "/edit/:email",
+			views: {
+				'main': {
+					templateUrl: 'templates/edit.html',
+					controller: 'PersonDetailController'
+				}
+			}
+		})
+		.state('create', {
+			url: "/create",
+			views: {
+				'main': {
+					templateUrl: 'templates/edit.html',
+					controller: 'PersonCreateController'
+				}
+			}
+		});
+
+	$urlRouterProvider.otherwise('/');
+});
+
 app.config(function($httpProvider, $resourceProvider, laddaProvider, $datepickerProvider) {
 	$httpProvider.defaults.headers.common['Authorization'] = "Token " + api_key;
 	$resourceProvider.defaults.stripTrailingSlashes = false;
